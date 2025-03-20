0028fd471fae6aabc3d21d592fd12e9505b72e75
rikuto-g-1 <rikuphonemen@gmail.com>
2025-03-20 18:02:21 +0900

    マイグレーションファイルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_17_170938) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_20_090057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
