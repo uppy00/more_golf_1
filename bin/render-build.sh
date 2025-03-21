343cf306e9722b822a6b3c6460e0bb5896509060
rikuto-g-1 <rikuphonemen@gmail.com>
2025-03-21 12:13:19 +0900

    bin/render-build.shの編集

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,4 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-# bundle exec rails db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
-bundle exec rake db:seed 
+bundle exec rails db:migrate
