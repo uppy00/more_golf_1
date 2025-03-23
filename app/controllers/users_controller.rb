11f11d32921059dc1437e46a0812be2912f64764
rikuto-g-1 <rikuphonemen@gmail.com>
2025-03-23 19:22:57 +0900

    ユーザー編集ページの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   # require_loginをnew及びcreateにのみスキップ
   skip_before_action :require_login, only: %i[new create]
+  before_action :set_user,only: %i[show edit update]
+
 
   def new
     @user = User.new
@@ -17,9 +19,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+  
   private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
+  # 表示するユーザーの情報を取得
+  def set_user
+    @user = current_user
+  end
+
 end
---
+++ db/migrate/20250323091103_add_my_page_fields_to_users.rb
@@ -0,0 +1,13 @@
+class AddMyPageFieldsToUsers < ActiveRecord::Migration[7.2]
+  def change
+    add_column :users, :nickname, :string
+    add_column :users, :self_introduction, :text
+    add_column :users, :favorite_course, :string
+    add_column :users, :favorite_diving_range, :string
+    add_column :users, :driving_range_type, :string
+    add_column :users, :driving_range_price, :integer
+    add_column :users, :best_score, :integer
+    add_column :users, :best_score_course, :string
+    add_column :users, :favorite_video_creator, :string
+  end
+end
