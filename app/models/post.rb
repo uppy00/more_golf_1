359558606dfd5cc5805cc26b8db8da1623c61448
rikuto-g-1 <rikuphonemen@gmail.com>
2025-03-25 16:18:33 +0900

    写真の投稿機能と表示機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
 
+  mount_uploader :image, PostImageUploader
   belongs_to :user
 end
