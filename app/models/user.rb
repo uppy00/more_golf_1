279f2dd78604859105a187a0479f430728712f0d
rikuto-g-1 <rikuphonemen@gmail.com>
2025-04-12 16:40:02 +0900

    プロフィールのバリデーションエラー表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,14 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] && password_confirmation.present? }
   # userのmypageのカラム↓
   validates :nickname, presence: true, length: { maximum: 20 }, uniqueness: true, unless: -> { new_record? }
-  validates :self_introduction, length: { maximum: 200 }
+  validates :self_introduction, length: { maximum: 150 }
   validates :favorite_course, length: { maximum: 20 }, allow_nil: true
   validates :favorite_driving_range, length: { maximum: 30 }, allow_nil: true
   validates :driving_range_type, inclusion: { in: [ "打ち放題", "球数", "インドア" ], allow_nil: true }
   validates :driving_range_price, numericality: { only_integer: true }, allow_nil: true
   validates :best_score, numericality: { only_integer: true }, allow_nil: true
-  validates :best_score_course, length: { maximum: 100 }, allow_nil: true
-  validates :favorite_video_creator, length: { maximum: 100 }, allow_nil: true
+  validates :best_score_course, length: { maximum: 30 }, allow_nil: true
+  validates :favorite_video_creator, length: { maximum: 20 }, allow_nil: true
 
   has_many :posts, dependent: :destroy
 
