3f37eeb47776e1cbc531f25c9fa46bf4454d4e93
rikuto-g-1 <rikuphonemen@gmail.com>
2025-04-12 20:41:23 +0900

    投稿詳細の表示整頓

--- db/schema.rb
+++ db/schema.rb
@@ -38,8 +38,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_12_084314) do
     t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
-    t.string "first_name", null: false
-    t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
