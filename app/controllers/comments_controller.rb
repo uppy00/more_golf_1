9670f49390af023b711b3c80ba2f2174bb9293e0
rikuto-g-1 <rikuphonemen@gmail.com>
2025-04-12 19:26:27 +0900

    コメント機能の作成

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+  
+  def create
+    # ポストに紐づいた新しいコメントを作成
+    @comment = @post.comments.build(comment_params)  
+    # コメントしたユーザーを設定（ログインしているユーザー）
+    @comment.user = current_user  
+
+    if @comment.save
+      flash[:success] = "コメントを投稿しました"
+      redirect_to @post
+    else
+      flash[:danger] = "コメントの投稿に失敗しました"
+      redirect_to @post
+    end
+  end
+
+  private
+  # コメントを投稿するポストを取得
+  def set_post
+    @post = Post.find(params[:post_id]) 
+  end
+  # コメント本文のみ受け取る
+  def comment_params
+    params.require(:comment).permit(:body)  
+  end
+end
