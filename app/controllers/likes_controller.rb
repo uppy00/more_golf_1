a27286cc20a09c18c9e3db4d298ee1e5353b8688
rikuto-g-1 <rikuphonemen@gmail.com>
2025-05-16 15:30:58 +0900

    いいねのパーシャル化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,21 +1,15 @@
 class LikesController < ApplicationController
-  before_action :set_post
-
   def create
-    current_user.like(@post)
+    post = Post.find(params[:post_id])
+    current_user.like(post)
     flash[:notice] = "いいねしました"
     redirect_to posts_path
   end
 
   def destroy
-    current_user.unlike(@post)
+    post = Post.find(params[:post_id])
+    current_user.unlike(post)
     flash[:notice] = "いいねを削除しました"
     redirect_to posts_path
-  end
-
-  private
-
-  def set_post
-    @post = Post.find(params[:post_id])
   end
 end
