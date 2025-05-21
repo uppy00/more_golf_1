b4c08233fdbfb87d8be0b6f808433ad353d0f998
rikuto-g-1 <rikuphonemen@gmail.com>
2025-05-21 17:04:32 +0900

    like_countの実施

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,27 @@
 class LikesController < ApplicationController
+  before_action :set_post
+
   def create
-    @post = Post.find(params[:post_id])
     current_user.like(@post)
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to posts_path }
+    end
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
     current_user.unlike(@post)
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to posts_path }
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
