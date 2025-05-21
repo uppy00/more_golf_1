a4a4eded7e5ca10fcbf6cdca4d28cb9fb844f84b
rikuto-g-1 <rikuphonemen@gmail.com>
2025-05-21 15:44:32 +0900

    いいね機能ajax化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,11 @@
 class LikesController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    current_user.like(post)
-    flash[:notice] = "いいねしました"
-    redirect_to posts_path
+    @post = Post.find(params[:post_id])
+    current_user.like(@post)
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    current_user.unlike(post)
-    flash[:notice] = "いいねを削除しました"
-    redirect_to posts_path
+    @post = Post.find(params[:post_id])
+    current_user.unlike(@post)
   end
 end
