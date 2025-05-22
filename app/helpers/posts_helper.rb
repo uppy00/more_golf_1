3a0186a57e9ebd44c9ced6b60a1c80d8b6d4bee4
rikuto-g-1 <rikuphonemen@gmail.com>
2025-05-22 17:43:32 +0900

    tag機能の完成

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+  def tag_color_class(tag_name)
+    case tag_name
+    when "スコア記録" then "bg-green-500"
+    when "練習記録" then "bg-blue-500"
+    when "質問" then "bg-yellow-500"
+    when "その他" then "bg-gray-500"
+    end
+  end
 end
