3a6302da22bdbcbb7409ca678817f7fd384e7948
rikuto-g-1 <rikuphonemen@gmail.com>
2025-05-31 15:55:23 +0900

    paginationの表示を整理

--- db/schema.rb
+++ db/schema.rb
@@ -57,8 +57,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_22_065739) do
     t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
-    t.string "first_name", null: false
-    t.string "last_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
