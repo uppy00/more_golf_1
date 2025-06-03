374bb1075475a0bff5be806e941db525544ed9d2
rikuto-g-1 <rikuphonemen@gmail.com>
2025-06-03 18:09:20 +0900

    タグによる入力フォームの切り替え完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,15 +19,28 @@ class PostsController < ApplicationController
   end
   # 投稿の作成
   def create
+    tag = Tag.find_by(id: params[:post][:tag_id])
+    postable = case tag&.name
+              when "スコア記録"
+                ScoreRecord.new(postable_params)
+              when "練習記録"
+                PracticeRecord.new(postable_params)
+              else
+                nil
+              end
+  
     @post = current_user.posts.build(post_params)
+    @post.tag = tag
+    @post.postable = postable if postable.present?
+  
     if @post.save
-      flash[:success] = "投稿に成功しました"
-      redirect_to posts_path
+      redirect_to posts_path, notice: "投稿に成功しました"
     else
-      flash.now[:danger] = "投稿に失敗しました。エラーメッセージを確認してください"
+      flash.now[:danger] = "投稿に失敗しました"
       render :new, status: :unprocessable_entity
     end
   end
+  
   # 投稿の詳細
   def show
     @post = Post.find(params[:id])
@@ -77,12 +90,13 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(
-      :title, :body, :image, :tag_id,
-      postable_attributes: [
-        :id, :couse_name, :score,
-        :driving_range_name, :practice_hour, :ball_count, :effort_focus, :video_reference, :type
-      ]
+    params.require(:post).permit(:title, :body, :image, :tag_id )
+  end
+
+  def postable_params
+    params.require(:post).fetch(:postable_attributes, {}).permit(
+      :course_name, :score,
+      :driving_range_name, :practice_hour, :ball_count, :effort_focus, :video_reference
     )
   end
 end
--- db/migrate/20250531080523_add_postable_to_posts.rb
+++ db/migrate/20250531080523_add_postable_to_posts.rb
@@ -1,5 +1,5 @@
 class AddPostableToPosts < ActiveRecord::Migration[7.2]
   def change
-    add_reference :posts, :postable, polymorphic: true, null: false
+    add_reference :posts, :postable, polymorphic: true, null: true
   end
 end
