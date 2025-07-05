0e3d09ab40cc32ca7930ec8c3912af5911331191
rikuto-g-1 <rikuphonemen@gmail.com>
2025-07-05 17:59:22 +0900

    letter_opener_webのinitializersを訂正

--- config/initializers/letter_opener_web.rb
+++ config/initializers/letter_opener_web.rb
@@ -1,5 +1,8 @@
-Rails.application.config.to_prepare do
-  LetterOpenerWeb::LettersController.class_eval do
-    skip_before_action :verify_authenticity_token
+if Rails.env.development? || Rails.env.test?
+  Rails.application.config.to_prepare do
+    LetterOpenerWeb::LettersController.class_eval do
+      skip_before_action :verify_authenticity_token
+    end
   end
 end
+
