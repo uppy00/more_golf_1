0d8126c968e802ed0bddccb6d4505c0a8a0fc578
rikuto-g-1 <rikuphonemen@gmail.com>
2025-07-21 14:58:18 +0900

    tagがなくてもエラーが出ないようにした

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,5 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-# bundle exec rails db:migrate
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create db:migrate db:seed
+bundle exec rails db:migrate
 bundle exec rails db:seed
