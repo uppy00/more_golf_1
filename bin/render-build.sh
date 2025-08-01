8c4ca774ac0c52124f4bfe15c14b3ad9904d3aae
rikuto-g-1 <rikuphonemen@gmail.com>
2025-08-01 19:35:49 +0900

    render-build.shの変更

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,13 @@
+#!/usr/bin/env bash
 set -o errexit
 
 bundle install
+yarn install
+
+# JS と CSS をビルド
+yarn build
+yarn build:css
+
+# Rails アセットプリコンパイル
 bundle exec rails assets:precompile
-bundle exec rails assets:clean
 bundle exec rails db:migrate
-bundle exec rails db:seed
