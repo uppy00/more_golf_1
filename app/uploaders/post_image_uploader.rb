6ea23585e5128ac4093e32a805ce1108879453da
rikuto-g-1 <rikuphonemen@gmail.com>
2025-08-02 13:32:12 +0900

    lint_fix

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -52,7 +52,7 @@ class PostImageUploader < CarrierWave::Uploader::Base
       img
     end
   end
-  
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
