d146d002e5fff93e642e0734247d652b68cc7991
rikuto-g-1 <rikuphonemen@gmail.com>
2025-08-05 16:59:50 +0900

    comment Rspecの完成

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    it 'すべての属性が揃っていれば有効' do
+      comment = build(:comment)
+      expect(comment).to be_valid
+    end
+
+    it 'contentが空だと無効' do
+      comment = build(:comment, body: "")
+      expect(comment).not_to be_valid
+      expect(comment.errors[:body]).to include("を入力してください").or include("can't be blank")
+    end
+
+    it 'userが紐づいていなければ無効' do
+      comment = build(:comment, user: nil)
+      expect(comment).not_to be_valid
+    end
+
+    it 'postが紐づいていなければ無効' do
+      comment = build(:comment, post: nil)
+      expect(comment).not_to be_valid
+    end
+  end
+
+  describe 'アソシエーション動作（削除の影響）' do
+    let!(:comment) { create(:comment) }
+
+    it 'ユーザー削除時にコメントも削除される' do
+      expect { comment.user.destroy }.to change { Comment.count }.by(-1)
+    end
+
+    it '投稿削除時にコメントも削除される' do
+      expect { comment.post.destroy }.to change { Comment.count }.by(-1)
+    end
+  end
 end
