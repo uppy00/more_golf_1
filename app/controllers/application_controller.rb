e4c5bb804a329ae76f64872577045ed1ca94f0e0
rikuto-g-1 <rikuphonemen@gmail.com>
2025-10-05 15:17:31 +0900

    古いサファリで対応するように

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
   # ユーザーがログインしているかどうかを判断　してない場合not_authenticatedに指定したルートにリダイレクト
   before_action :require_login, unless: :high_voltage_static_page?
   # 携帯でgoogleログインを実装するためのもの
