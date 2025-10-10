0056dd6dd74774d1a5a6d62fc45bc3c60908831a
rikuto-g-1 <rikuphonemen@gmail.com>
2025-10-10 15:46:47 +0900

    portの変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,12 +85,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
-    port:                 587,
+    port:                 465,
     domain:               "moregolf-life.com",
     user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       "plain",
-    enable_starttls_auto: true,
+    ssl:                  true,         
     open_timeout:         30,
     read_timeout:         30
   }
