c55510048051e8a1bcee73e1d0fbc4aeab70db20
rikuto-g-1 <rikuphonemen@gmail.com>
2025-11-17 18:03:02 +0900

    youtubeのURLを貼ると動画が表示されるようにした。

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -7,4 +7,15 @@ module PostsHelper
     when "その他" then "bg-gray-500"
     end
   end
+  def youtube_embed_id(str)
+    s = str.to_s.strip
+    return "" if s.blank?
+
+    return s if s.length == 11
+    return s.split("youtu.be/").last.split("?").first if s.include?("youtu.be/")
+    return s.split("v=").last.split("&").first       if s.include?("v=")
+    return s.split("/shorts/").last.split("?").first if s.include?("/shorts/")
+
+    ""
+  end
 end
